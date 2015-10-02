73d3b0c0454412f08e0da98ea631f0c6647c20b8
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-02 13:29:33 +0530

    Battery status file added

---
+++ battery_status.py
@@ -0,0 +1,27 @@
+import wmi
+import time
+import winsound
+
+setBatteryLowLimit = 20
+
+def battery_low():
+    for i in range(1, 10):
+        winsound.Beep(i * 100, 200)
+
+def battery_check():
+    battery = wmi.WMI().Win32_Battery()[0]
+    print 'Current battery status is ' + str(battery.EstimatedChargeRemaining)'%'
+    print 'You will be notified when battery is below '+str(setBatteryLowLimit)+'%'
+    while(1):
+        battery = wmi.WMI().Win32_Battery()[0]
+        if battery.EstimatedChargeRemaining < setBatteryLowLimit:
+            battery_low()
+            print 'Battery below ' + str(setBatteryLowLimit) + '%! Connect charger!'
+            break
+        time.sleep(120)
+
+def set_limit(x):
+    if isinstance(x,int) and x>=0 and x <=100:
+        setBatteryLowLimit = x
+    else:
+        print 'Please enter an integer between 0 and 100'
