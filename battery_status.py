ed99c78df4a28aa2321f70db31caf4e11c50fdab
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-02 14:02:38 +0530

    Variable names changed. Check status added. Comments added.

--- battery_status.py
+++ battery_status.py
@@ -2,13 +2,20 @@ import wmi
 import time
 import winsound
 
+# Default value of Battery Low Limit
 setBatteryLowLimit = 20
 
+# Function to echo sound when battery is lower than setBatteryLowLimit
+# parameters: None
+# returns: None
 def battery_low():
     for i in range(1, 10):
         winsound.Beep(i * 100, 200)
 
-def battery_check():
+# Function to start battery status notifier
+# parameters: None
+# returns: None
+def battery_status():
     battery = wmi.WMI().Win32_Battery()[0]
     print 'Current battery status is ' + str(battery.EstimatedChargeRemaining)'%'
     print 'You will be notified when battery is below '+str(setBatteryLowLimit)+'%'
@@ -20,12 +27,20 @@ def battery_check():
             break
         time.sleep(120)
 
-def set_limit(x):
-    if isinstance(x,int) and x>=0 and x <=100:
-        setBatteryLowLimit = x
+# Function to set Battery Low Limit
+# parameters:{
+#     limit: Integer between 0 and 100 both inclusive
+# }
+# returns: None
+def set_limit(limit=""):
+    if isinstance(limit,int) and limit >=0 and limit <=100:
+        setBatteryLowLimit = limit
     else:
         print 'Please enter an integer between 0 and 100'
 
+# Function to check status of battery
+# parameters: None
+# returns: None
 def check_status():
     battery = wmi.WMI().Win32_Battery()[0]
     print 'Your current battery status is ' + str(battery.EstimatedChargeRemaining)'%'
