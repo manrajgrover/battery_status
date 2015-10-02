7ada1b22e3e71003c7bb3ea0b9562ede8e683d9d
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-02 16:19:47 +0530

    Access to commands from terminal added.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='battery_status',
-      version='0.0.8',
+      version='0.0.9',
       description='Battery Status Notifier',
       long_description='Check your battery status and add a notifier on Windows',
       keywords='battery status notifier windows check battery status python',
@@ -9,6 +9,12 @@ setup(name='battery_status',
       author='Manraj Singh',
       author_email='manrajsinghgrover@gmail.com',
       license='MIT',
+      entry_points = {
+          'console_scripts': [
+               'check_battery_status=battery_status.command_line:check_status',
+               'start_battery_notifier=battery_status.command_line:battery_notif',
+          ],
+      },
       packages=['battery_status'],
       install_requires=[
             'wmi',
